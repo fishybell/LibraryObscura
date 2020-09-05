118c440764dde4f298758b5f687a76ee12748299
Nathan Bell <nathan.bell@berkadia.com>
2020-09-05 08:58:03 -0600

    more vm capabilities

--- vm.c
+++ vm.c
@@ -5,21 +5,36 @@
 #define BUFFER_SIZE 1024
 
 struct vm {
+  void *pointers[256];
   int registers[256];
   int loops[256];
   char *buffers[256];
 };
+
+typedef void(*void_func)(void*);
 
 int parse_line(struct vm *v, char *line) {
   int i = 0;
   char key = line[2];
   char key2 = line[4];
   char key3 = line[6];
+  void_func func;
+  void *param;
   //printf("line[0]: %c\n", line[0]);
   //printf("key: %c\n", key);
   //printf("key2: %c\n", key2);
   //printf("key3: %c\n", key3);
   switch (line[0]) {
+    case 'u': // point a pointer to a buffer
+      param = v->buffers[key2];
+      v->pointers[key] = param;
+      break;
+    case 'x': // exec a function at a pointer
+      func = v->pointers[key];
+      param = v->pointers[key2];
+      func(param);
+      break;
+
     case 'r': // set a register
       v->registers[key] = key2;
       break;
@@ -36,13 +51,13 @@ int parse_line(struct vm *v, char *line) {
       printf("%c", v->registers[key]);
       break;
 
-    case 'e': // end a loop (moving up)
+    case 'e': // end a loop (incrementing iterator)
       if (++v->loops[key] < key2 - '0') {
         return -1 * (key3 - '0');
       }
       break;
 
-    case 'f': // end a loop (moving down)
+    case 'f': // end a loop (decrementing iterator)
       if (--v->loops[key] > key2 - '0') {
         return -1 * (key3 - '0');
       }
@@ -54,11 +69,11 @@ int parse_line(struct vm *v, char *line) {
         v->buffers[key][i] = line[4+i];
         i++;
       }
-      
+
       v->registers[key] = key2;
       break;
 
-    case 'c': // pring a buffer
+    case 'c': // print a buffer
       printf("%s", v->buffers[key]);
       break;
   }
@@ -66,49 +81,83 @@ int parse_line(struct vm *v, char *line) {
   return 1; // default of one step forward
 }
 
-int main () {
-	printf("Should print like so\n");
+void some_func(void *ptr) {
+  for (int j=3; j>0; j--)
+    printf("%s%c", (char*)ptr, '3'+j);
+}
 
-  for (int i=0; i<3; i++) {
-    printf("%c", 'a'+i);
-    for (int j=3; j>0; j--)
-      printf("%c", '3'+j);
-    printf("\n");
-  }
-  printf("done...\n");
-
-  char *lines[] = {
-    "r c a",   // set register c to 'a'
-    "l i 0",   // set loop iterator i to 0
-    // {
-      "a d c i", // add register c to i, store in d
-      "p d",     // print register d
-      
-      "r e 3",   // set register e to '1'
-      "l j 3",   // set loop iterator j to 3
-      // {
-        "a d e j", // add register e to j, store in d
-        "p d",     // print register d
-        "f j 0 2", // decrement loop iterator j, check against 0, go back 2 steps or move on
-        "r d \n",   // set register d to '\n'
-      // }
-
-      "p d",     // print register d
-      "e i 3 9", // increment loop iterator i, check against 3, go back 2 steps or move on
-    // }
-
-    "b a done...\n",  // set buffer a to 'done'
-    "c a",         // print buffer a
-    NULL
-  };
-
-  struct vm v;
-
+void parse_lines(struct vm *v, char **lines) {
   int i = 0;
   while (lines[i] != NULL) {
     //printf("i: %d\n", i);
-    i += parse_line(&v, lines[i]);
+    i += parse_line(v, lines[i]);
   }
+}
+
+struct vm *new_standard_vm() {
+  struct vm *v = malloc(sizeof(struct vm));
+
+  v->pointers['0'] = &some_func;
+
+  return v;
+}
+
+int main () {
+	printf("Should print like so\n");
+
+  printf("-----------------\n");
+
+  // test block {
+  for (int i=0; i<3; i++) {
+    printf("%c", 'a'+i);
+    for (int j=3; j>0; j--)
+      printf("\nHi: %c", '3'+j);
+    printf("\n");
+  }
+  printf("done...\n");
+  // }
+
+  printf("-----------------\n");
+
+  char *lines[] = {
+    // setup outer loop
+    "r c a",                         // set register c to 'a'
+    "l i 0",                         // set loop iterator i to 0
+
+    // {
+      // print next letter in outer loop
+      "a d c i",                     // add register c to i, store in d
+      "p d",                         // print register d
+
+      // call external function (inner loop)
+      "b a \nHi: ",                  // set buffer a to '\nHi: '
+      "u h a",                       // point pointer h to buffer a
+      "x 0 h",                       // execute function 0 with pointer h
+
+      // print carraige, return, end outer loop
+      "r d \n",                      // set register d to '\n'
+      "p d",                         // print register d
+      "e i 3 7",                     // increment loop iterator i, check against 3, go back to start of loop
+    // }
+
+    // test noop
+    "",                              // noop
+    "",                              // noop
+
+    // test forward jump
+    "l i .",                         // set loop iterator i to -2 (minimum for this trick)
+    "e i 0 -",                       // increment loop iterator i, check against 0, jump backwards -3 ('0' - '-' == -3)
+    "b a wrong...\n",                // set buffer a to 'wrong...\n'
+    "c a",                           // print buffer a
+
+    // finish up
+    "b a done...\n",                 // set buffer a to 'done...\n'
+    "c a",                           // print buffer a
+    NULL
+  };
+
+  parse_lines(new_standard_vm(), lines);
+  printf("-----------------\n");
 
   return 0;
 }
