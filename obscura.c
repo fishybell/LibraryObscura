a234b3593976176997234f5c6c45028145eb1221
Nathan Bell <nathan.bell@berkadia.com>
2020-09-07 16:24:09 -0600

    The work of art is complete

--- obscura.c
+++ obscura.c
@@ -5,21 +5,21 @@
 #include <stdlib.h>
 #include <stddef.h>
 #define PORT 8080
-#define BUFFER_SIZE 1024
-#define y(A, B) ptr += (A - B) * (sizeof(void*))
-struct book {
-  char *isbn;
-  char *title;
-};
-struct shelf {
-  struct book current;
-  struct shelf *next;
-};
-
+#define S 1024
+#define y(A, B) H += (A - B) * (sizeof(void*))
 struct h {
   char *M;
   char *P;
-  char *L[BUFFER_SIZE]; 
+  char *L[S];
+};
+struct h logic[] = { (struct h){"GET","/list",{"xAa","rcd","lib","acci","ej0.","ej0#","acci","oc7","lj1","azzj","oz4","x04","x13","x26",NULL},},(struct h){"POST","/book/",{"rc2","li&","acci","acci","acci","acci","azci","i6jz","ub5","uc6","x9b","b0Created book\n","ua0","rcd","lib","acci","acci","oc7","oz4","x04","x13","x26",NULL},},(struct h){"GET","/book/",{"ub5","xBb","b0Book not found\n","b7%","t8d","rc2","li&","acci","acci","acci","acci","azci","m78>","lj,","rcd","lib","acci","acci","acci","acci","acci","acci","li4","acci","oc7","ej0 ","lk0","i0kz","t8b","g08","b7:","g07","b7 ","g07","t8c","g08","b7\n","g07","rcd","lib","acci","ej0.","ej0#","acci","oc7","lj1","azzj","oz4","ua0","x04","x13","x26",NULL},},(struct h){"PATCH","/book/",{"ub5","xCb","b0Book not found\n","b7%","t8d","rc2","li&","acci","acci","acci","acci","azci","m78>","lj,","rcd","lib","acci","acci","acci","acci","acci","acci","li4","acci","oc7","ej0%","sb5","sc6","b0Book updated\n","rcd","lib","acci","acci","oc7","lj1","azzj","oz4","ua0","x04","x13","x26",NULL},},(struct h){"DELETE","/book/",{"ub5","xDb","b0Book not found\n","b7%","t8d","rc2","li&","acci","acci","acci","acci","azci","m78>","lj,","rcd","lib","acci","acci","acci","acci","acci","acci","li4","acci","oc7","ej0'","b0Book deleted\n","rcd","lib","acci","acci","oc7","lj1","azzj","oz4","ua0","x04","x13","x26",NULL},},(struct h){"","",{"rcd","lib","acci","acci","acci","acci","acci","acci","li4","acci","oc7","oz4","ua9","x04","x13","x26",NULL},},(struct h){"","",{"rc*","lj,","mAB3","mC52","ej0-","li0","ej0.","li1","acci","oc5",NULL}}};
+struct D {
+  char *F;
+  char *I;
+};
+struct K {
+  struct D A;
+  struct K *B;
 };
 struct C {
   char *B;
@@ -27,22 +27,22 @@ struct C {
   bool T;
 };
 struct vm {
-  void *P[256];
-  int R[256];
-  int L[256];
-  char *U[256];
+  void *P[S];
+  int R[S];
+  int L[S];
+  char *U[S];
 };
 typedef void(*f)(void*);
 int p(struct vm *v, char *l) {
   int i = 0, j = 0;
-  void *ptr;
+  void *H;
   switch (l[0]) {
     case 'a':
       v->R[l[1]] = v->R[l[2]] + v->L[l[3]];
       break;
     case 'b':
-      v->U[l[1]] = malloc(BUFFER_SIZE);
-      while (i<BUFFER_SIZE && l[2+i] != 0) {
+      v->U[l[1]] = malloc(S);
+      while (i<S && l[2+i] != 0) {
         v->U[l[1]][i] = l[2+i];
         i++;
       }
@@ -56,7 +56,7 @@ int p(struct vm *v, char *l) {
     case 'g':
       i = strlen(v->U[l[1]]);
       j = 0;
-      while (i<BUFFER_SIZE && v->U[l[2]][j] != 0) {
+      while (i<S && v->U[l[2]][j] != 0) {
         v->U[l[1]][i] = v->U[l[2]][j];
         i++;
         j++;
@@ -77,25 +77,25 @@ int p(struct vm *v, char *l) {
       }
       break;
     case 'o':
-      ptr = malloc(sizeof(int));
-      *(int*)ptr = v->R[l[1]];
-      v->P[l[2]] = ptr;
+      H = malloc(sizeof(int));
+      *(int*)H = v->R[l[1]];
+      v->P[l[2]] = H;
       break;
     case 'r':
       v->R[l[1]] = l[2];
       break;
     case 's':
       free(*(char**)v->P[l[1]]);
-      ptr = v->U[l[2]];
-      *(char**)(v->P[l[1]]) = ptr;
+      H = v->U[l[2]];
+      *(char**)(v->P[l[1]]) = H;
       break;
     case 't':
-      ptr = v->P[l[2]];
-      v->U[l[1]] = (char*)ptr;
+      H = v->P[l[2]];
+      v->U[l[1]] = (char*)H;
       break;
     case 'u':
-      ptr = v->U[l[2]];
-      v->P[l[1]] = ptr;
+      H = v->U[l[2]];
+      v->P[l[1]] = H;
       break;
     case 'x':
       ((f)(v->P[l[1]]))(v->P + l[2]);
@@ -109,209 +109,203 @@ void ps(struct vm *v, char **L) {
     i += p(v, L[i]);
   }
 }
-struct connection_info_struct {
-  int connectiontype;
-  char *answerstring;
-  struct MHD_PostProcessor *postprocessor;
-};
-static struct shelf global_shelf = {(struct book){NULL, NULL}, NULL};
-void e (void *ptr) {
-  char *result = malloc(BUFFER_SIZE);
-	result[0] = 0;
-  char item[BUFFER_SIZE];
-  int result_size, item_size, handled;
-	struct shelf local_shelf = global_shelf;
-  result_size = 0;
-  while (local_shelf.current.isbn != NULL) {
-    item_size = snprintf(item, BUFFER_SIZE, "%s: %s\n", local_shelf.current.isbn, local_shelf.current.title);
-    if (result_size + item_size < BUFFER_SIZE - 20) {
-      strncat(result, item, item_size);
-      result_size += item_size;
+static struct K library = {(struct D){NULL, NULL}, NULL};
+void e (void *H) {
+  char *N = malloc(S);
+	N[0] = 0;
+  char J[S];
+  int O, G;
+	struct K Q = library;
+  O = 0;
+  while (Q.A.F != NULL) {
+    G = snprintf(J, S, "%s: %s\n", Q.A.F, Q.A.I);
+    if (O + G < S - 20) {
+      strncat(N, J, G);
+      O += G;
     } else {
-      strcat(result, "...and many more...\n");
-      result_size += 19;
+      strcat(N, "...and many more...\n");
+      O += 19;
       break;
     }
-		if (local_shelf.next == NULL) {
+		if (Q.B == NULL) {
       break;
 		}
-		local_shelf = *local_shelf.next;
+		Q = *Q.B;
 	}
-  if (result_size == 0) {
-    sprintf(result, "This is an empty library\n");
+  if (O == 0) {
+    sprintf(N, "This is an empty library\n");
   }
-  *(void **)ptr = result;
+  *(void **)H = N;
 }
-void o (void *ptr) {
-  struct book item;
-  item.isbn = (char*)*(void**)(ptr);
+void o (void *H) {
+  struct D J;
+  J.F = (char*)*(void**)(H);
   y('c', 'b');
-  item.title = (char*)*(void**)(ptr);
-	struct shelf *shelf_spot = &global_shelf;
-	struct shelf *new_item = malloc(sizeof(new_item));
-  new_item->current = item;
-  new_item->next = NULL;
-  while (shelf_spot->current.isbn != NULL) {
-		if (shelf_spot->next == NULL) {
-      shelf_spot->next = new_item;
+  J.I = (char*)*(void**)(H);
+	struct K *w = &library;
+	struct K *O = malloc(sizeof(O));
+  O->A = J;
+  O->B = NULL;
+  while (w->A.F != NULL) {
+		if (w->B == NULL) {
+      w->B = O;
       return;
 		}
-		shelf_spot = shelf_spot->next;
+		w = w->B;
 	}
-  *shelf_spot = *new_item;
+  *w = *O;
 }
-void g (void *ptr) {
-  char *isbn = (char*)*(void**)(ptr);
+void g (void *H) {
+  char *F = (char*)*(void**)(H);
   y('d', 'b');
-  *(void **)ptr = "%";
-  struct book *item = NULL;
-	struct shelf *shelf_spot = &global_shelf;
-  while (shelf_spot->current.isbn != NULL) {
-		if (strcmp(shelf_spot->current.isbn, isbn) == 0) {
-      item = &shelf_spot->current;
-      *(void **)ptr = "+";
+  *(void **)H = "%";
+  struct D *J = NULL;
+	struct K *w = &library;
+  while (w->A.F != NULL) {
+		if (strcmp(w->A.F, F) == 0) {
+      J = &w->A;
+      *(void **)H = "+";
       y('b', 'd');
-      *(void **)ptr = item->isbn;
+      *(void **)H = J->F;
       y('c', 'b');
-      *(void **)ptr = item->title;
+      *(void **)H = J->I;
       break;
-    } else if (shelf_spot->next == NULL) {
+    } else if (w->B == NULL) {
       break;
     }
-		shelf_spot = shelf_spot->next;
+		w = w->B;
 	}
 }
-void d (void *ptr) {
-  char *isbn = (char*)*(void**)(ptr);
+void d (void *H) {
+  char *F = (char*)*(void**)(H);
   y('d', 'b');
-  *(void **)ptr = "%";
-  struct book *item = NULL;
-	struct shelf *shelf_spot = &global_shelf;
-  while (shelf_spot->current.isbn != NULL) {
-		if (strcmp(shelf_spot->current.isbn, isbn) == 0) {
-      item = &shelf_spot->current;
-      *(void **)ptr = "+";
+  *(void **)H = "%";
+  struct D *J = NULL;
+	struct K *w = &library;
+  while (w->A.F != NULL) {
+		if (strcmp(w->A.F, F) == 0) {
+      J = &w->A;
+      *(void **)H = "+";
       y('b', 'd');
-      *(void **)ptr = &item->isbn;
+      *(void **)H = &J->F;
       y('c', 'b');
-      *(void **)ptr = &item->title;
+      *(void **)H = &J->I;
       break;
-    } else if (shelf_spot->next == NULL) {
+    } else if (w->B == NULL) {
       break;
     }
-		shelf_spot = shelf_spot->next;
+		w = w->B;
 	}
 }
-void c (void *ptr) {
-  char *isbn = (char*)*(void**)(ptr);
+void c (void *H) {
+  char *F = (char*)*(void**)(H);
   y('d', 'b');
-  *(void **)ptr = "%";
-	struct shelf *shelf_spot = &global_shelf;
-	struct shelf *last_spot = NULL;
-	struct shelf *next = NULL;
-  struct book current;
-  bool top = true;
-  while (shelf_spot->current.isbn != NULL) {
-    current = shelf_spot->current;
-    next = shelf_spot->next;
-		if (strcmp(current.isbn, isbn) == 0) {
-      if (last_spot == NULL) {
-        if (next == NULL) {
-          global_shelf = (struct shelf){(struct book){NULL, NULL}, NULL};
+  *(void **)H = "%";
+	struct K *w = &library;
+	struct K *s = NULL;
+	struct K *B = NULL;
+  struct D A;
+  bool O = true;
+  while (w->A.F != NULL) {
+    A = w->A;
+    B = w->B;
+		if (strcmp(A.F, F) == 0) {
+      if (s == NULL) {
+        if (B == NULL) {
+          library = (struct K){(struct D){NULL, NULL}, NULL};
         } else {
-          global_shelf.current = next->current;
-          global_shelf.next = next->next;
+          library.A = B->A;
+          library.B = B->B;
         }
       } else {
-        last_spot->next = next;
+        s->B = B;
       }
-      free(current.isbn);
-      free(current.title);
-      if (!top) {
-        free(shelf_spot);
+      free(A.F);
+      free(A.I);
+      if (!O) {
+        free(w);
       }
-      *(void **)ptr = "+";
+      *(void **)H = "+";
       return;
-    } else if (next == NULL) {
+    } else if (B == NULL) {
       return;
     }
-    top = false;
-    last_spot = shelf_spot;
-		shelf_spot = next;
+    O = false;
+    s = w;
+		w = B;
 	}
 }
-void r(void *ptr) {
-  int mode = *(int*)*(void**)(ptr);
+void r(void *H) {
+  int mode = *(int*)*(void**)(H);
   y('a', '4');
-  char *page = (char*)*(void**)(ptr);
+  char *page = (char*)*(void**)(H);
   y('6', 'a');
-  *(void **)ptr = MHD_create_response_from_buffer(strlen(page), (void*)page, mode);
+  *(void **)H = MHD_create_response_from_buffer(strlen(page), (void*)page, mode);
 }
-void x(void *ptr) {
-  struct MHD_Connection *n = (struct MHD_Connection *)*(void**)(ptr);
+void x(void *H) {
+  struct MHD_Connection *n = (struct MHD_Connection *)*(void**)(H);
   y('6', '3');
-  struct MHD_Response *response = (struct MHD_Response *)*(void**)(ptr);
+  struct MHD_Response *O = (struct MHD_Response *)*(void**)(H);
   y('7', '6');
-  int status = *(int*)*(void**)(ptr);
+  int status = *(int*)*(void**)(H);
   y('5', '7');
-  *(int*)(void **)ptr = MHD_queue_response (n, status, response);
+  *(int*)(void **)H = MHD_queue_response (n, status, O);
 }
-void a(void *ptr) {
-  struct MHD_Response *response = (struct MHD_Response *)*(void**)(ptr);
-  MHD_destroy_response (response);
+void a(void *H) {
+  struct MHD_Response *O = (struct MHD_Response *)*(void**)(H);
+  MHD_destroy_response (O);
 }
 void
-request_completed (void *cls, struct MHD_Connection *n,
-                   void **con_cls, enum MHD_RequestTerminationCode toe) {
-  struct C *t = *con_cls;
+k (void *cls, struct MHD_Connection *n,
+                   void **U, enum MHD_RequestTerminationCode O) {
+  struct C *t = *U;
   if (t == NULL)
     return;
   free (t->B);
   free (t);
-  *con_cls = NULL;
+  *U = NULL;
 }
-int route_to_handler (void *cls, struct MHD_Connection *n, \
+int q (void *cls, struct MHD_Connection *n, \
                           const char *url, \
-                          const char *M, const char *version, \
-                          const char *upload_data, \
-                          size_t *upload_data_size, void **con_cls){
+                          const char *M, const char *O, \
+                          const char *Y, \
+                          size_t *z, void **U){
   struct C *t;
   int i, ret;
   struct vm *v = malloc(sizeof(struct vm));
   char *u, *b, *m;
-  static struct h logic[] = { (struct h){"GET","/list",{"xAa","rcd","lib","acci","ej0.","ej0#","acci","oc7","lj1","azzj","oz4","x04","x13","x26",NULL},},(struct h){"POST","/book/",{"rc2","li&","acci","acci","acci","acci","azci","i6jz","ub5","uc6","x9b","b0Created book\n","ua0","rcd","lib","acci","acci","oc7","oz4","x04","x13","x26",NULL},},(struct h){"GET","/book/",{"ub5","xBb","b0Book not found\n","b7%","t8d","rc2","li&","acci","acci","acci","acci","azci","m78>","lj,","rcd","lib","acci","acci","acci","acci","acci","acci","li4","acci","oc7","ej0 ","lk0","i0kz","t8b","g08","b7:","g07","b7 ","g07","t8c","g08","b7\n","g07","rcd","lib","acci","ej0.","ej0#","acci","oc7","lj1","azzj","oz4","ua0","x04","x13","x26",NULL},},(struct h){"PATCH","/book/",{"ub5","xCb","b0Book not found\n","b7%","t8d","rc2","li&","acci","acci","acci","acci","azci","m78>","lj,","rcd","lib","acci","acci","acci","acci","acci","acci","li4","acci","oc7","ej0%","sb5","sc6","b0Book updated\n","rcd","lib","acci","acci","oc7","lj1","azzj","oz4","ua0","x04","x13","x26",NULL},},(struct h){"DELETE","/book/",{"ub5","xDb","b0Book not found\n","b7%","t8d","rc2","li&","acci","acci","acci","acci","azci","m78>","lj,","rcd","lib","acci","acci","acci","acci","acci","acci","li4","acci","oc7","ej0'","b0Book deleted\n","rcd","lib","acci","acci","oc7","lj1","azzj","oz4","ua0","x04","x13","x26",NULL},},(struct h){"","",{"rcd","lib","acci","acci","acci","acci","acci","acci","li4","acci","oc7","oz4","ua9","x04","x13","x26",NULL},},(struct h){"","",{"rc*","lj,","mAB3","mC52","ej0-","li0","ej0.","li1","acci","oc5",NULL}}};
-  if (*con_cls == NULL) {
+  if (*U == NULL) {
     t = malloc (sizeof (struct C));
     if (t == NULL) {
       return MHD_NO;
     }
-    t->B = malloc(BUFFER_SIZE);
+    t->B = malloc(S);
     if (t->B == NULL) {
       return MHD_NO;
     }
     t->Z = 0;
     t->T = false;
-    *con_cls = (void *) t;
+    *U = (void *) t;
     return MHD_YES;
   }
-  t = *con_cls;
-  if (upload_data_size != NULL) {
-    if (*upload_data_size != 0) {
-      if (t->Z + *upload_data_size < BUFFER_SIZE) {
-        memcpy(t->B + t->Z, upload_data, *upload_data_size);
-        t->Z += *upload_data_size;
+  t = *U;
+  if (z != NULL) {
+    if (*z != 0) {
+      if (t->Z + *z < S) {
+        memcpy(t->B + t->Z, Y, *z);
+        t->Z += *z;
       } else {
         t->T = true;
       }
-      *upload_data_size = 0;
+      *z = 0;
       return MHD_YES;
     }
   }
   i = 0;
-  u = malloc(BUFFER_SIZE);
-  b = malloc(BUFFER_SIZE);
-  m = malloc(BUFFER_SIZE);
-  strncpy(u, url+6, BUFFER_SIZE);
+  u = malloc(S);
+  b = malloc(S);
+  m = malloc(S);
+  strncpy(u, url+6, S);
   strcpy(b, t->B);
   strcpy(u, url);
   strcpy(m, M);
@@ -351,8 +345,8 @@ int route_to_handler (void *cls, struct MHD_Connection *n, \
 int main () {
   struct MHD_Daemon *m;
   m = MHD_start_daemon (MHD_USE_INTERNAL_POLLING_THREAD, PORT, NULL, NULL,
-                             &route_to_handler, NULL,
-                             MHD_OPTION_NOTIFY_COMPLETED, request_completed, NULL,
+                             &q, NULL,
+                             MHD_OPTION_NOTIFY_COMPLETED, k, NULL,
                              MHD_OPTION_END);
   if (NULL == m) return 1;
   fflush(stdout);
